d73f201639b28769948f47f349775f8b75a3d3aa
Gustag16 <gu.amadeu022@gmail.com>
2026-06-20 18:29:13 -0300

    feat: mapa leaflet

--- frontend/script.js
+++ frontend/script.js
@@ -24,3 +24,11 @@ botao.addEventListener("click", () => {
         `Visualização: ${tipo}\nArquivo: ${arquivoSelecionado.name}`
     );
 });
+
+// mapa do Brasil
+var map = L.map('map').setView([-15.4, -55], 4);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
